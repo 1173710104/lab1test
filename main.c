5dcae7fb8e883f1fae2f88762f1d9ecc9b551a75
1173710104 <765816432@qq.com>
2019-10-24 20:56:04 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    float f;
+
+    for(;;)
+    {
+        printf("请输入一个浮点数：");
+        scanf("%f",&f);
+        printf("这个浮点数的值是%f\n",f);
+        if(f==0)break;
+    }
+    return 0;
+}
+
